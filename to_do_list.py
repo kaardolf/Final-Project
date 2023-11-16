c7a6f0f050e09756902ba3c9d82aa9ac1000fc08
kaardolf <kaardolf@mavs.coloradomesa.edu>
2023-11-16 17:27:13 +0000

    created path

---
+++ to_do_list.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo.errors import OperationFailure
+
+path_to_certificate = 'cert.pem'
+
+uri = "mongodb+srv://cluster0.7kjaziu.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+client = MongoClient(uri,
+                     tls=True,
+                     tlsCertificateKeyFile=path_to_certificate,
+                     server_api=ServerApi('1'))
+db = client['final-project']
+collection = db['to_do']
+doc_count = collection.count_documents({})
+print(doc_count)
+
