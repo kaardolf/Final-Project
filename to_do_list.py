0b7181eca72b499102671de81f4a1ef294b0520c
kaardolf <kaardolf@mavs.coloradomesa.edu>
2023-11-17 17:42:35 +0000

    added staten island (4)

--- to_do_list.py
+++ to_do_list.py
@@ -16,6 +16,9 @@ print(doc_count)
 
 to_do = []
 
+#11 Brooklyn
+#1 Staten
+#2 Bronx
 resturant_1 = {"restaurant_id":"40356018","borough":"Brooklyn","cuisine":"American",}
 
 to_do.append(resturant_1)
@@ -27,12 +30,22 @@ to_do.append(resturant_2)
 resturant_3 = {"name":"Kosher Island", "borough":"Staten Island","cuisine":"Jewish/Kosher"}
 
 to_do.append(resturant_3)
+resturant_4 = {"name":"Bagels N Buns","borough":"Staten Island","cuisine":"Delicatessen"}
 
-resturant_4 = {"name ": "Wendy'S", "borough" : "Brooklyn", "cuisine" : "Hamburgers"}
+to_do.append(resturant_4)
 
-resturant_5 = {"name":"Morris Park Bake Shop", "borough":"Bronx","cuisine":"Bakery"}
+resturant_5 = {"name":"Carvel Ice Cream","borough":"Staten Island","cuisine":"Ice Cream, Gelato, Yogurt, Ices"}
 
-resturant_6 = {"name":"Tov Kosher Kitchen","borough":"Queens","cuisine":"Jewish/Kosher"}
+to_do.append(resturant_5)
+
+resturant_6 = {"name":"B & M Hot Bagel & Grocery","borough":"Staten Island","cuisine":"Delicatessen"}
+to_do.append(resturant_6)
+
+resturant_12 = {"name ": "Wendy'S", "borough" : "Brooklyn", "cuisine" : "Hamburgers"}
+
+resturant_13 = {"name":"Morris Park Bake Shop", "borough":"Bronx","cuisine":"Bakery"}
+
+resturant_14 = {"name":"Tov Kosher Kitchen","borough":"Queens","cuisine":"Jewish/Kosher"}
 
 resturant_7 = {"name":"Brunos On The Boulevard","borough":"Queens","cuisine":"American"}
 
@@ -43,6 +56,8 @@ resturant_9 = {"name":"Taste The Tropics Ice Cream","borough":"Brooklyn","cuisin
 resturant_10 = {"name":"Wild Asia","borough":"Bronx","cuisine":"American"}
 
 resturant_11 = {"name":"C & C Catering Service","borough":"Brooklyn","cuisine":"American"}
+
+
 try:
     collection.insert_many(to_do)
 except OperationFailure as ex:
