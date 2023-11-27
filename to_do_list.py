5c01c9a8e6d29ab222d03635e2ee06dbf2b3dc98
kaardolf <kaardolf@mavs.coloradomesa.edu>
2023-11-27 23:24:03 +0000

    determined the resturants

--- to_do_list.py
+++ to_do_list.py
@@ -16,52 +16,62 @@ print(doc_count)
 
 to_do = []
 
-#11 Brooklyn
-#1 Staten
-#2 Bronx
+#American Manhattan- Reif's travern
+#Done #American Brooklyn- Regina Caterers
+#American Queens- Maloney's bar
+#Pizza Staten Island- Denino's Pizzeria Tavern
+#Pizza Queens- Jack's Pizza and pasta
+#Pizza Brooklyn- Fascati's Pizzeria
+#Irish Manhattan - Killarney Rose
+#Irish Queens - O'Hanlon's Pub
+#Irish Bronx - Mcdwyers Rub (121)
+#Italian Queens - Piccola Venezia
+#Italian Staten Island - Marina Cafe
+#Italian Manhattan - Forlinis Resturant
+#Mexican Manhattan - Cuchifrito
+#Mexican Brooklyn - Casa Pepe
+#Mexican Manhattan - Mexico Lindo Resturant
+#Done #Delicatessen Staten Island - Plaza bagels and deli
+#Done #Delicatessen Brooklyn - Wilken's Fine food
+#Done #Delicatessen Staten Island - B & M Hot Bagel and Grocery
+
+#Update to be "name":"Rivviera Caterer" not "restaurant_id":"40356018"
 resturant_1 = {"restaurant_id":"40356018","borough":"Brooklyn","cuisine":"American",}
 
 to_do.append(resturant_1)
-
+#Keep
 resturant_2 = {"name":"Wilken'S Fine Food", "borough":"Brooklyn","cuisine":"Delicatessen"}
 
 to_do.append(resturant_2)
 
+#Remove
 resturant_3 = {"name":"Kosher Island", "borough":"Staten Island","cuisine":"Jewish/Kosher"}
 
 to_do.append(resturant_3)
+
+#keep but name is Plaza bagels and deli
 resturant_4 = {"name":"Bagels N Buns","borough":"Staten Island","cuisine":"Delicatessen"}
 
 to_do.append(resturant_4)
 
+#remove
 resturant_5 = {"name":"Carvel Ice Cream","borough":"Staten Island","cuisine":"Ice Cream, Gelato, Yogurt, Ices"}
 
 to_do.append(resturant_5)
 
+#keep
 resturant_6 = {"name":"B & M Hot Bagel & Grocery","borough":"Staten Island","cuisine":"Delicatessen"}
 to_do.append(resturant_6)
 
-resturant_12 = {"name ": "Wendy'S", "borough" : "Brooklyn", "cuisine" : "Hamburgers"}
 
-resturant_13 = {"name":"Morris Park Bake Shop", "borough":"Bronx","cuisine":"Bakery"}
 
-resturant_14 = {"name":"Tov Kosher Kitchen","borough":"Queens","cuisine":"Jewish/Kosher"}
-
-resturant_7 = {"name":"Brunos On The Boulevard","borough":"Queens","cuisine":"American"}
-
-resturant_8 = {"name":"Regina Caterers","borough":"Brooklyn","cuisine":"American"}
-
-resturant_9 = {"name":"Taste The Tropics Ice Cream","borough":"Brooklyn","cuisine":"Ice Cream, Gelato, Yogurt, Ices"}
-
-resturant_10 = {"name":"Wild Asia","borough":"Bronx","cuisine":"American"}
-
-resturant_11 = {"name":"C & C Catering Service","borough":"Brooklyn","cuisine":"American"}
 
 
 try:
     collection.insert_many(to_do)
 except OperationFailure as ex:
     raise ex
+
 
 
 
