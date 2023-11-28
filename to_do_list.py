caafbf567b21d3991a446e3590fafc6bea437d94
kaardolf <kaardolf@mavs.coloradomesa.edu>
2023-11-28 17:39:55 +0000

    all needed resturants

--- to_do_list.py
+++ to_do_list.py
@@ -16,52 +16,61 @@ print(doc_count)
 
 to_do = []
 
-#"name":"Reif's travern", "borough":"Manhattan","cuisine":"American"
-#Done #American Brooklyn- Regina Caterers
-#"name":"Maloney's bar", "borough":"Queens","cuisine":"American"
-#"name":"Denino's Pizzeria Tavern", "borough":"Staten Island","cuisine":"Pizza"
-#"name":"Jack's Pizza and pasta", "borough":"Queens","cuisine":"Pizza"
-#"name":"Fascati's Pizzeria", "borough":"Brooklyn","cuisine":"Pizza"
-#"name":"Killarney Rose", "borough":"Manhattan","cuisine":"Irish"
-#"name":"O'Hanlon's Pub", "borough":"Queens","cuisine":"Irish"
-#"name":"Mcdwyers Rub", "borough":"Bronx","cuisine":"Irish"
-#"name":"Piccola Venezia","borough":"Queens","cuisine":"Italian"
-#"name":"Marina Cafe","borough":"Staten Island","cuisine":"Italian"
-#"name": "Forlinis Resturant", "borough":"Manhattan","cuisine":"Italian"
-#"name":"Cuchifrito","borough":"Manhattan","cuisine":"Mexican"
-#"name":"Casa Pepe","borough":"Brooklyn","cuisine":"Mexican"
-#"name":"Mexico Lindo Resturant","borough":"Manhattan","cuisine":"Mexican"
-#Done #Delicatessen Staten Island - Plaza bagels and deli
-#Done #Delicatessen Brooklyn - Wilken's Fine food
-#Done #Delicatessen Staten Island - B & M Hot Bagel and Grocery
-
-#Update to be "name":"Rivviera Caterer" not "restaurant_id":"40356018"
-resturant_1 = {"restaurant_id":"40356018","borough":"Brooklyn","cuisine":"American",}
-
+resturant_1 = {"name":"Reif's travern", "borough":"Manhattan","cuisine":"American"}
 to_do.append(resturant_1)
-#Keep
-resturant_2 = {"name":"Wilken'S Fine Food", "borough":"Brooklyn","cuisine":"Delicatessen"}
 
+resturant_2 = {"name":"Regina Caterers","borough":"Brooklyn","cuisine":"American"}
 to_do.append(resturant_2)
 
-#Remove
-resturant_3 = {"name":"Kosher Island", "borough":"Staten Island","cuisine":"Jewish/Kosher"}
-
+resturant_3 = {"name":"Maloney's bar", "borough":"Queens","cuisine":"American"}
 to_do.append(resturant_3)
 
-#keep but name is Plaza bagels and deli
-resturant_4 = {"name":"Bagels N Buns","borough":"Staten Island","cuisine":"Delicatessen"}
-
+resturant_4 = {"name":"Denino's Pizzeria Tavern", "borough":"Staten Island","cuisine":"Pizza"}
 to_do.append(resturant_4)
 
-#remove
-resturant_5 = {"name":"Carvel Ice Cream","borough":"Staten Island","cuisine":"Ice Cream, Gelato, Yogurt, Ices"}
-
+resturant_5 = {"name":"Jack's Pizza and pasta", "borough":"Queens","cuisine":"Pizza"}
 to_do.append(resturant_5)
 
-#keep
-resturant_6 = {"name":"B & M Hot Bagel & Grocery","borough":"Staten Island","cuisine":"Delicatessen"}
+resturant_6 = {"name":"Fascati's Pizzeria", "borough":"Brooklyn","cuisine":"Pizza"}
 to_do.append(resturant_6)
+
+resturant_7 = {"name":"Killarney Rose", "borough":"Manhattan","cuisine":"Irish"}
+to_do.append(resturant_7)
+
+resturant_8 = {"name":"O'Hanlon's Pub", "borough":"Queens","cuisine":"Irish"}
+to_do.append(resturant_8)
+
+resturant_9 = {"name":"Mcdwyers Rub", "borough":"Bronx","cuisine":"Irish"}
+to_do.append(resturant_9)
+
+resturant_10 = {"name":"Piccola Venezia","borough":"Queens","cuisine":"Italian"}
+to_do.append(resturant_10)
+
+resturant_11 = {"name":"Marina Cafe","borough":"Staten Island","cuisine":"Italian"}
+to_do.append(resturant_11)
+
+resturant_12 = {"name": "Forlinis Resturant", "borough":"Manhattan","cuisine":"Italian"}
+to_do.append(resturant_12)
+
+resturant_13 = {"name":"Cuchifrito","borough":"Manhattan","cuisine":"Mexican"}
+to_do.append(resturant_13)
+
+resturant_14 = {"name":"Casa Pepe","borough":"Brooklyn","cuisine":"Mexican"}
+to_do.append(resturant_14)
+
+resturant_15 = {"name":"Mexico Lindo Resturant","borough":"Manhattan","cuisine":"Mexican"}
+to.append(resturant_15)
+
+
+resturant_16 = {"name":"Wilken'S Fine Food", "borough":"Brooklyn","cuisine":"Delicatessen"}
+to_do.append(resturant_16)
+
+
+resturant_17 = {"name":"Plaza bagels and deli","borough":"Staten Island","cuisine":"Delicatessen"}
+to_do.append(resturant_17)
+
+resturant_18 = {"name":"B & M Hot Bagel & Grocery","borough":"Staten Island","cuisine":"Delicatessen"}
+to_do.append(resturant_18)
 
 
 
